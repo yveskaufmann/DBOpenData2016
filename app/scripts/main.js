997f1e4a34607d053a92ba3a792c7d53bb86c7c4
Yves Kaufmann <fxdapokalypse@googlemail.com>
2016-06-18 14:31:33 +0200

    Replace locate button with default

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,9 +7,6 @@
 	function CityBikeMap() {
 
 		this.$map = $('#map');
-		this.$enterLocationView = $('.enterLocation');
-		this.$locateMeButton = $('.locateMeButton');
-		this.$alert = $('.alert');
 		this.heatmapCfg = {
 			radius: 0.1,
 			maxOpacity: .6,
@@ -17,8 +14,7 @@
 			useLocalExtrema: true,
 			latField: 'lat',
 			lngField: 'lng',
-			valueField: 'count',
-
+			valueField: 'count'
 		};
 		this.heatmapInCfg = {
 			radius: 0.3,
@@ -38,38 +34,13 @@
 			lngField: 'lng',
 			valueField: 'count'
 		};
-		$(window).on('hashchange', (() => {
-			this.start();
-		}).bind(this));
-
-		this.retrieveCurrentLocation();
 	}
 
 	CityBikeMap.prototype.start = function () {
-		this.locationURL = window.location.hash;
-		this.locationURL = this.locationURL.slice(1) || null;
-
-		this.$enterLocationView.show();
-		this.$locateMeButton
-			.unbind()
-			.click(this.retrieveCurrentLocation.bind(this))
-				.on('touchstart', this.retrieveCurrentLocation.bind(this));
-
-		if (this.locationURL != null) {
-			this.initShowLocationView();
-		}
+		this.initShowLocationView();
 	};
 
 	CityBikeMap.prototype.initShowLocationView = function () {
-		var location = this.locationURL.split('|');
-		if (location.length !== 2 || !location.every(jQuery.isNumeric)) {
-			$('.alert').show();
-			return;
-		}
-
-		if (this.map) {
-			this.map.remove();
-		}
 
 		this.osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 			attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -83,7 +54,7 @@
 			zoom: 10,
 			maxZoom: 19,
 			minZoom: 3,
-			center: location,
+			center: [52.505, 13.09],
 			layers: [
 				this.osmLayer,
 				this.heatmapLayer,
@@ -91,11 +62,6 @@
 				this.heatmapOutLayer
 			]
 		});
-
-		/* add a example locations */
-		L.marker(location)
-			.addTo(this.map)
-			.bindPopup('kk');
 
 		/* add layer switch control */
 		var mainLayer = {
@@ -107,42 +73,26 @@
 			'Ankommende Fahrräder': this.heatmapInLayer,
 			'Ausgehende Fahrräder': this.heatmapOutLayer
 		};
-	
-		
+
+
 
 		let layerOptions = {collapsed: false};
 		L.control.layers(mainLayer, heatLayer, layerOptions).addTo(this.map);
+		this.locateControl = L.control.locate({
+			position: "topleft",
+			keepCurrentZoomLevel: false,
+			locateOptions: {
+				maxZoom: 10
+			}
+		}).addTo(this.map);
 
 		var heatMapData = {
 			max: 8,
 			data: [{lat: location[0], lng: location[1], count: 5}]
 		};
-		this.heatmapLayer.setData(heatMapData);
+		// this.heatmapLayer.setData(heatMapData);
 
-		this.$map.show();
-		this.map.invalidateSize();
-
-	};
-
-	CityBikeMap.prototype.retrieveCurrentLocation = function () {
-
-		if (!navigator.geolocation) {
-			window.alert('Geolocation isn\'t supported by your browser.');
-		} else {
-			navigator.geolocation.getCurrentPosition(this.goToLocation.bind(this));
-		}
-	};
-
-	CityBikeMap.prototype.goToLocation = function (location) {
-
-		var baseURL = window.location.href;
-		var hashPos = baseURL.indexOf('#');
-		if ( hashPos > 0 ) {
-			baseURL = baseURL.slice(0, hashPos);
-		}
-
-		var url = baseURL + '#' + location.coords.latitude + '|' + location.coords.longitude;
-		window.location.href = url;
+		this.locateControl.start();
 	};
 
 	this.cityBikeMap = new CityBikeMap();
